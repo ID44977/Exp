ab5a9ddeb46540aa236ce323c4b9000e0ffe79ad
ID44977 <760305100@qq.com>
2018-11-21 15:48:01 +0800

    ver1.0

--- src/p20181121/Account.java
+++ src/p20181121/Account.java
@@ -1,15 +1,17 @@
 package p20181121;
 
+import javax.swing.*;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Account {
+    JTextArea textArea = new JTextArea(30, 50);
     private Lock accountLock = new ReentrantLock();
     private double balance = 10000; // 账户余额
     public void withdraw(double money) {
         accountLock.lock();
         double newBalance = balance - money;
-        System.out.println(balance + "元  " + money + "元  " + newBalance + "元\n");
+        textArea.setText(textArea.getText() + balance + "元  " + money + "元  " + newBalance + "元\n");
         try {
             Thread.sleep(10); // 模拟此业务需要一段处理时间
         } catch (InterruptedException e) {
@@ -20,6 +22,9 @@ public class Account {
     }
     public double getBalance() {
         return balance;
+    }
+    public void showBalance(){
+        textArea.setText(textArea.getText() + "账户余额: " + getBalance() + "元\n");
     }
 }
 
